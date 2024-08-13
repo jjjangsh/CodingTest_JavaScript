9af35cab4a114747b6d46a2a08f4d79289c5047f
jjjangsh <hoko7022@gmail.com>
2024-08-13 09:46:09 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -0,0 +1,10 @@
+function solution(numbers) {
+    let sum = 0;
+    
+    for(let i = 0; i < 10; i++) {
+        if(!numbers.includes(i)) {
+            sum += i;
+        }
+    }
+    return sum;
+}
