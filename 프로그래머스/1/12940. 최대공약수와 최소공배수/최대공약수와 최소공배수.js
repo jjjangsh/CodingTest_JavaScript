ae23c6b761794c6030c0167bba1eb21fa9b0c510
jjjangsh <hoko7022@gmail.com>
2024-08-14 21:44:17 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,21 +1,13 @@
-function solution(n, m) {
-    let gdc = 1;
-    let lcm = 1;
-    let gdcAndLcm = [];
-    
-    for(let i = 2; i <= Math.min(n, m); i++) {
-        if(n % i === 0 && m % i === 0) {
-            gdc = i;
+function solution(a, b) {
+    let gcd = (a, b) => {
+        while (b !== 0) {
+            [a, b] = [b, a % b];
         }
-    }
-    
-    while(true) {
-        if(lcm % n === 0 && lcm % m === 0) {
-            break;
-        }
-        lcm++;
-    }
-    
-    gdcAndLcm.push(gdc, lcm);
-    return gdcAndLcm;
+        return a;
+    };
+
+    let gcdValue = gcd(a, b); // 최대공약수
+    let lcmValue = (a * b) / gcdValue; // 최소공배수
+
+    return [gcdValue, lcmValue];
 }
